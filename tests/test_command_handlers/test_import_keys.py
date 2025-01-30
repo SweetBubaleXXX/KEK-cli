c6068b39b16aa41d1172ca85526e91061ac129e9
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2025-01-30 17:55:39 +0300

    Test import multiple keys at once

--- tests/test_command_handlers/test_import_keys.py
+++ tests/test_command_handlers/test_import_keys.py
@@ -9,6 +9,7 @@ from gnukek_cli.command_handlers.import_keys import ImportKeysContext, ImportKey
 from gnukek_cli.constants import CONFIG_FILENAME
 from gnukek_cli.keys import get_private_key_filename, get_public_key_filename
 from tests.constants import (
+    ENCRYPTED_KEY_BATCH,
     ENCRYPTED_PRIVATE_KEY,
     KEY_ENCRYPTION_PASSWORD,
     KEY_ID,
@@ -84,3 +85,24 @@ def test_import_single_key(
         assert private_key_path.exists()
         assert settings["default"] == KEY_ID
         assert settings["private"] == [KEY_ID]
+
+
+def test_import_multiple_keys(create_handler, password_prompt_mock, storage_dir):
+    password_prompt_mock.get_password.side_effect = [
+        password for _, password in ENCRYPTED_KEY_BATCH
+    ]
+
+    context = ImportKeysContext(
+        key_files=[BytesIO(key) for key, _ in ENCRYPTED_KEY_BATCH]
+    )
+    handle = create_handler(context)
+    handle()
+
+    assert password_prompt_mock.get_password.call_count == len(ENCRYPTED_KEY_BATCH)
+
+    config_path = storage_dir / CONFIG_FILENAME
+    with open(config_path, "r") as config_file:
+        settings = json.load(config_file)
+
+    assert len(settings["private"]) == len(ENCRYPTED_KEY_BATCH)
+    assert len(settings["public"]) == len(ENCRYPTED_KEY_BATCH)
